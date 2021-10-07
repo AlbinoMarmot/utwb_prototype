1200df0005f26dd4528cf18267c54cebb98b2cd3
CornerSky6525 <86079315+CornerSky6525@users.noreply.github.com>
2021-10-06 23:20:33 -0400

    Adding enemies
    Added enemy script and set up nav mesh

---
+++ Until The Whistle Blows/Assets/Scripts/Movement/Enemy/Enemy.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform Pdest, Sdest;
+    NavMeshAgent agent;
+   public bool reached;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+       // Sdest = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (reached)
+        {
+            agent.destination = Sdest.position;
+            if(transform.position == Sdest.position)
+            {
+                Invoke("PatrolOngoing", 2);
+            }
+        }
+        else
+        {
+            agent.destination = Pdest.position;
+        }
+        if(transform.position == Pdest.position)
+        {
+            Invoke("PatrolFinished", 2);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("GameOver");
+        }
+    }
+    void PatrolFinished()
+    {
+        reached = true;
+    }
+    void PatrolOngoing()
+    {
+        reached = false;
+    }
+    void GameOver()
+    {
+
+    }
+}
